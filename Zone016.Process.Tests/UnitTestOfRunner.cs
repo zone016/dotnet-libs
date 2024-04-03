3f9a8182f2b6a9b02385198e48330f2673903dc1
Jojo <jonas.uliana@passwd.com.br>
2024-04-02 21:11:17 -0300

    chore(Process): format issue

--- Zone016.Process.Tests/UnitTestOfRunner.cs
+++ Zone016.Process.Tests/UnitTestOfRunner.cs
@@ -1,3 +1,4 @@
+﻿// Copyright (c) Zone016 Hackerspace. All Rights Reserved. Licensed under the MIT license.
 
 using System.Text;
 
@@ -12,18 +13,18 @@ public class UnitTestOfRunner
         var shell = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
             ? "cmd.exe"
             : "/usr/bin/sh";
-        
+
         var commands = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
             ? new[] { "/c", "dir" }
             : ["-c", "ls"];
-    
+
         var buffer = new StringBuilder();
         await Runner.Create(shell)
             .WithArguments(commands)
             .WithOutputDataReceivedCallback(stdout => buffer.Append(stdout))
-            .WithTimeout(TimeSpan.FromSeconds(10)) 
+            .WithTimeout(TimeSpan.FromSeconds(10))
             .RunAsync();
-        
+
         Assert.IsTrue(buffer.Length > 0);
     }
 }
