c7098e77bc22fe8ffb75dfbcb697e1c593933182
Jojo <jonas.uliana@passwd.com.br>
2024-04-02 21:05:49 -0300

    chore(Process): add tests

---
+++ Zone016.Process.Tests/UnitTestOfRunner.cs
@@ -0,0 +1,29 @@
+
+using System.Text;
+
+namespace Zone016.Process.Tests;
+
+[TestClass]
+public class UnitTestOfRunner
+{
+    [TestMethod]
+    public async Task TestRun()
+    {
+        var shell = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? "cmd.exe"
+            : "/usr/bin/sh";
+        
+        var commands = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? new[] { "/c", "dir" }
+            : ["-c", "ls"];
+    
+        var buffer = new StringBuilder();
+        await Runner.Create(shell)
+            .WithArguments(commands)
+            .WithOutputDataReceivedCallback(stdout => buffer.Append(stdout))
+            .WithTimeout(TimeSpan.FromSeconds(10)) 
+            .RunAsync();
+        
+        Assert.IsTrue(buffer.Length > 0);
+    }
+}
