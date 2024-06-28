96f6e41f03be947c9a88a213f42c00d955472842
Jojo <jonas.uliana@passwd.com.br>
2024-06-28 00:48:18 -0300

    chore: delete paw project
    I need to think more in how to have it implemented. Funny, is easier to do in Linux.

--- src/Zone016.Paw/Models/Contexts/SettingsContext.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Zone016 Hackerspace. All Rights Reserved. Licensed under the MIT license.
-
-namespace Zone016.Paw.Models.Contexts;
-
-[JsonSourceGenerationOptions(WriteIndented = true)]
-[JsonSerializable(typeof(Settings))]
-[JsonSerializable(typeof(SettingsAction))]
-internal partial class SettingsContext : JsonSerializerContext;
--- src/Zone016.Paw/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Zone016 Hackerspace. All Rights Reserved. Licensed under the MIT license.
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.EnsureEnvironmentSetup();
-builder.AddServices();
-builder.AddSingletons();
-
-var host = builder.Build();
-host.Run();
